0922d4feba07b2371d6c51b7725bee791c388a60
ttakuma17 <ttanimiya@gmail.com>
2022-03-31 23:57:56 +0900

    ts-basic-first

---
+++ src/world.ts
@@ -0,0 +1,12 @@
+export default class World {
+  message: string
+  constructor(message: string) {
+    this.message = message
+  }
+
+  sayhello(elem: HTMLElement | null) {
+    if (elem) {
+      elem.innerText = this.message
+    }
+  }
+}
